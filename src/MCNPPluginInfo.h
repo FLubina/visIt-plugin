01161315eb59a965558e00d2c465baf7fdfdab2b
Fran Lubina <=>
2023-08-21 17:09:19 +0200

    add source

--- src/MCNPPluginInfo.h
+++ src/MCNPPluginInfo.h
@@ -3,7 +3,7 @@
 // details.  No copyright assignment is required to contribute to VisIt.
 
 // ****************************************************************************
-//  MCNPPluginInfo.h
+//                               MCNPPluginInfo.h
 // ****************************************************************************
 
 #ifndef MCNP_PLUGIN_INFO_H
@@ -48,6 +48,7 @@ class MCNPCommonPluginInfo : public virtual CommonDatabasePluginInfo, public vir
     virtual DatabaseType              GetDatabaseType();
     virtual avtDatabase              *SetupDatabase(const char * const *list,
                                                     int nList, int nBlock);
+    virtual std::string               GetLicense() const;
 };
 
 class MCNPMDServerPluginInfo : public virtual MDServerDatabasePluginInfo, public virtual MCNPCommonPluginInfo
