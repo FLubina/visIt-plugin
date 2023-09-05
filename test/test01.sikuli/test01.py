650f2d73a7fa3921a63e869fff7857ab090f09f3
Fran <flubina123@gmail.com>
2023-09-05 18:17:36 +0200

    test02

---
+++ test/test01.sikuli/test01.py
@@ -0,0 +1,18 @@
+click("1693929770598.png")
+wait(1)
+click("1693929770598.png")
+wait(1)
+click("1693929796633.png")
+for i in range(7):
+    type(Key.DOWN)
+type(Key.RIGHT)
+for i in range(5):
+    type(Key.DOWN)
+type(Key.ENTER)
+click("1693930618556.png")
+click("1693929770598.png")
+wait(1)
+
+click("1693929796633.png")
+type(Key.UP)
+type(Key.ENTER)
