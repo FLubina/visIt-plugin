a29a9166d9eb8278c9164ab50bdc2454c2ed7485
Fran <flubina123@gmail.com>
2023-09-05 18:01:25 +0200

    test00

---
+++ test/test00_meshtal_mcnp.sikuli/test00_meshtal_mcnp.py
@@ -0,0 +1,20 @@
+Debug.on(True)
+click("1693927761090.png")
+click("1693929187794.png")
+for _ in range(3):
+    type(Key.DOWN)
+type(Key.ENTER)
+for _ in range(5):
+    type(Key.DOWN)
+type(Key.ENTER)
+click("1693929011732.png")
+type(Key.DOWN)
+type(Key.ENTER)
+click("1693929497308.png")
+for _ in range(9):
+    type(Key.DOWN)
+type(Key.RIGHT)
+for _ in range(2):
+    type(Key.DOWN)
+type(Key.ENTER)
+click("1693929646322.png")
