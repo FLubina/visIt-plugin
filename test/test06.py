92227f3f8f05ad4b0abca80fb968464171195b9d
Fran <flubina123@gmail.com>
2023-09-06 16:05:07 +0200

    test06

---
+++ test/test06.py
@@ -0,0 +1,286 @@
+AddPlot("Mesh", "MCNP mesh", 1, 1)
+DrawPlots()
+SetActivePlots(1)
+SetActivePlots(1)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (0, 0, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.Foreground  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.OpaqueCustom  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.On  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (0, 0, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.Foreground  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.Background  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.On  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (0, 0, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.Foreground  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.OpaqueRandom  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.On  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (0, 0, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.Foreground  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.OpaqueRandom  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.On  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (0, 0, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.Foreground  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.Background  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.Auto  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (255, 204, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.MeshCustom  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.Background  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.Auto  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (255, 204, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.Foreground  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.Background  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.Auto  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (255, 204, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.Foreground  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.Background  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.Auto  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.Fast  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.150322, 0.147527, 0.977568)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.0435799, 0.988831, -0.142526)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (255, 204, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.Foreground  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.Background  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.Auto  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (255, 204, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.Foreground  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.Background  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.Auto  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 204, 0, 255)
+MeshAtts.smoothingLevel = MeshAtts.None  # None, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 1
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.150322, 0.147527, 0.977568)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.0435799, 0.988831, -0.142526)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.152646, 0.149891, 0.976848)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.0426169, 0.98851, -0.145021)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.152646, 0.149891, 0.976848)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.0426169, 0.98851, -0.145021)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# MAINTENANCE ISSUE: SetSuppressMessagesRPC is not handled in Logging.C. Please contact a VisIt developer.
+SaveSession("/home/imenko_prezimenovic/.visit/crash_recovery.12473.session")
+# MAINTENANCE ISSUE: SetSuppressMessagesRPC is not handled in Logging.C. Please contact a VisIt developer.
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.24064, 0.203153, 0.949116)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.0555288, 0.979131, -0.195498)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
