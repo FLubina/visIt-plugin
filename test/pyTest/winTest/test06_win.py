5c587565052a92cbbbb6f5f01cdd6570582abf09
Fran Lubina <=>
2023-09-20 18:18:30 +0200

    winTest 6 i 7

---
+++ test/pyTest/winTest/test06_win.py
@@ -0,0 +1,209 @@
+AddPlot("Mesh", "MCNP mesh", 1, 1)
+DrawPlots()
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (255, 255, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.MeshCustom  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.OpaqueCustom  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.Auto  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 0, 255, 255)
+MeshAtts.smoothingLevel = MeshAtts.NONE  # NONE, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 1
+SetPlotOptions(MeshAtts)
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (255, 255, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.MeshCustom  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.OpaqueCustom  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.On  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 0, 255, 255)
+MeshAtts.smoothingLevel = MeshAtts.NONE  # NONE, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 0.584314
+SetPlotOptions(MeshAtts)
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.309811, 0.55749, -0.770209)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.334971, 0.822127, 0.460329)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (-0.0143204, 0.75845, -0.651574)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.0331516, 0.650923, 0.75842)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (-0.0830794, 0.993106, -0.0826982)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.472689, 0.0337847, 0.880581)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (-0.0222896, 0.991642, 0.127083)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.519454, -0.120096, 0.846017)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.0129497, 0.977179, -0.212024)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.478778, 0.192214, 0.856636)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+MeshAtts = MeshAttributes()
+MeshAtts.legendFlag = 1
+MeshAtts.lineWidth = 0
+MeshAtts.meshColor = (255, 255, 0, 255)
+MeshAtts.meshColorSource = MeshAtts.MeshCustom  # Foreground, MeshCustom, MeshRandom
+MeshAtts.opaqueColorSource = MeshAtts.OpaqueCustom  # Background, OpaqueCustom, OpaqueRandom
+MeshAtts.opaqueMode = MeshAtts.On  # Auto, On, Off
+MeshAtts.pointSize = 0.05
+MeshAtts.opaqueColor = (255, 0, 255, 255)
+MeshAtts.smoothingLevel = MeshAtts.Fast  # NONE, Fast, High
+MeshAtts.pointSizeVarEnabled = 0
+MeshAtts.pointSizeVar = "default"
+MeshAtts.pointType = MeshAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+MeshAtts.showInternal = 0
+MeshAtts.pointSizePixels = 2
+MeshAtts.opacity = 0.584314
+SetPlotOptions(MeshAtts)
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (-0.0248619, 0.988882, -0.146612)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.288471, 0.133322, 0.948161)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (-0.0266598, 0.990555, 0.134498)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (-0.152841, -0.137003, 0.978708)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+OpenDatabase("localhost:C:\\Users\\B1llG\\Desktop\\fer knjige\\5sem\\projektR\\testData\\meshtal", 0)
+DeleteActivePlots()
---
+++ test/pyTest/winTest/test07_win.py
@@ -0,0 +1,319 @@
+AddOperator("ThreeSlice", 1)
+DrawPlots()
+OpenDatabase("localhost:C:\\Users\\B1llG\\Desktop\\fer knjige\\5sem\\projektR\\testData\\meshtal", 0)
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (-0.22395, 0.935747, 0.27244)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (0.332153, -0.189524, 0.923989)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (-0.759175, 0.541592, 0.361014)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (0.647393, 0.570922, 0.504906)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+AddOperator("SphereSlice", 1)
+DrawPlots()
+SetActivePlots(0)
+SetActivePlots(0)
+SphereSliceAtts = SphereSliceAttributes()
+SphereSliceAtts.origin = (30, 30, 0)
+SphereSliceAtts.radius = 30
+SetOperatorOptions(SphereSliceAtts, 1, 1)
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (-0.759175, 0.541592, 0.361014)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (0.647393, 0.570922, 0.504906)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (-0.27341, 0.347707, 0.896854)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (0.775713, 0.631033, -0.00817)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.234444, 0.475712, 0.847782)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (0.731877, 0.487619, -0.476008)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.110565, -0.390961, 0.913742)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (0.822927, 0.55153, 0.136406)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.263884, -0.896668, -0.355459)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (0.947962, 0.173032, 0.267261)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+OpenDatabase("localhost:C:\\Users\\B1llG\\Desktop\\fer knjige\\5sem\\projektR\\testData\\meshtal", 0)
+PseudocolorAtts = PseudocolorAttributes()
+PseudocolorAtts.scaling = PseudocolorAtts.Log  # Linear, Log, Skew
+PseudocolorAtts.skewFactor = 1
+PseudocolorAtts.limitsMode = PseudocolorAtts.OriginalData  # OriginalData, ActualData
+PseudocolorAtts.minFlag = 0
+PseudocolorAtts.min = 0
+PseudocolorAtts.useBelowMinColor = 0
+PseudocolorAtts.belowMinColor = (0, 0, 0, 255)
+PseudocolorAtts.maxFlag = 0
+PseudocolorAtts.max = 1
+PseudocolorAtts.useAboveMaxColor = 0
+PseudocolorAtts.aboveMaxColor = (0, 0, 0, 255)
+PseudocolorAtts.centering = PseudocolorAtts.Natural  # Natural, Nodal, Zonal
+PseudocolorAtts.colorTableName = "Default"
+PseudocolorAtts.invertColorTable = 0
+PseudocolorAtts.opacityType = PseudocolorAtts.FullyOpaque  # ColorTable, FullyOpaque, Constant, Ramp, VariableRange
+PseudocolorAtts.opacityVariable = ""
+PseudocolorAtts.opacity = 1
+PseudocolorAtts.opacityVarMin = 0
+PseudocolorAtts.opacityVarMax = 1
+PseudocolorAtts.opacityVarMinFlag = 0
+PseudocolorAtts.opacityVarMaxFlag = 0
+PseudocolorAtts.pointSize = 0.05
+PseudocolorAtts.pointType = PseudocolorAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+PseudocolorAtts.pointSizeVarEnabled = 0
+PseudocolorAtts.pointSizeVar = "default"
+PseudocolorAtts.pointSizePixels = 2
+PseudocolorAtts.lineType = PseudocolorAtts.Line  # Line, Tube, Ribbon
+PseudocolorAtts.lineWidth = 0
+PseudocolorAtts.tubeResolution = 10
+PseudocolorAtts.tubeRadiusSizeType = PseudocolorAtts.FractionOfBBox  # Absolute, FractionOfBBox
+PseudocolorAtts.tubeRadiusAbsolute = 0.125
+PseudocolorAtts.tubeRadiusBBox = 0.005
+PseudocolorAtts.tubeRadiusVarEnabled = 0
+PseudocolorAtts.tubeRadiusVar = ""
+PseudocolorAtts.tubeRadiusVarRatio = 10
+PseudocolorAtts.tailStyle = PseudocolorAtts.NONE  # NONE, Spheres, Cones
+PseudocolorAtts.headStyle = PseudocolorAtts.NONE  # NONE, Spheres, Cones
+PseudocolorAtts.endPointRadiusSizeType = PseudocolorAtts.FractionOfBBox  # Absolute, FractionOfBBox
+PseudocolorAtts.endPointRadiusAbsolute = 0.125
+PseudocolorAtts.endPointRadiusBBox = 0.05
+PseudocolorAtts.endPointResolution = 10
+PseudocolorAtts.endPointRatio = 5
+PseudocolorAtts.endPointRadiusVarEnabled = 0
+PseudocolorAtts.endPointRadiusVar = ""
+PseudocolorAtts.endPointRadiusVarRatio = 10
+PseudocolorAtts.renderSurfaces = 1
+PseudocolorAtts.renderWireframe = 0
+PseudocolorAtts.renderPoints = 0
+PseudocolorAtts.smoothingLevel = 0
+PseudocolorAtts.legendFlag = 1
+PseudocolorAtts.lightingFlag = 1
+PseudocolorAtts.wireframeColor = (0, 0, 0, 0)
+PseudocolorAtts.pointColor = (0, 0, 0, 0)
+SetPlotOptions(PseudocolorAtts)
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.263884, -0.896668, -0.355459)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (0.947962, 0.173032, 0.267261)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
+
+PseudocolorAtts = PseudocolorAttributes()
+PseudocolorAtts.scaling = PseudocolorAtts.Linear  # Linear, Log, Skew
+PseudocolorAtts.skewFactor = 1
+PseudocolorAtts.limitsMode = PseudocolorAtts.OriginalData  # OriginalData, ActualData
+PseudocolorAtts.minFlag = 0
+PseudocolorAtts.min = 0
+PseudocolorAtts.useBelowMinColor = 0
+PseudocolorAtts.belowMinColor = (0, 0, 0, 255)
+PseudocolorAtts.maxFlag = 0
+PseudocolorAtts.max = 1
+PseudocolorAtts.useAboveMaxColor = 0
+PseudocolorAtts.aboveMaxColor = (0, 0, 0, 255)
+PseudocolorAtts.centering = PseudocolorAtts.Natural  # Natural, Nodal, Zonal
+PseudocolorAtts.colorTableName = "Default"
+PseudocolorAtts.invertColorTable = 0
+PseudocolorAtts.opacityType = PseudocolorAtts.FullyOpaque  # ColorTable, FullyOpaque, Constant, Ramp, VariableRange
+PseudocolorAtts.opacityVariable = ""
+PseudocolorAtts.opacity = 1
+PseudocolorAtts.opacityVarMin = 0
+PseudocolorAtts.opacityVarMax = 1
+PseudocolorAtts.opacityVarMinFlag = 0
+PseudocolorAtts.opacityVarMaxFlag = 0
+PseudocolorAtts.pointSize = 0.05
+PseudocolorAtts.pointType = PseudocolorAtts.Point  # Box, Axis, Icosahedron, Octahedron, Tetrahedron, SphereGeometry, Point, Sphere
+PseudocolorAtts.pointSizeVarEnabled = 0
+PseudocolorAtts.pointSizeVar = "default"
+PseudocolorAtts.pointSizePixels = 2
+PseudocolorAtts.lineType = PseudocolorAtts.Line  # Line, Tube, Ribbon
+PseudocolorAtts.lineWidth = 0
+PseudocolorAtts.tubeResolution = 10
+PseudocolorAtts.tubeRadiusSizeType = PseudocolorAtts.FractionOfBBox  # Absolute, FractionOfBBox
+PseudocolorAtts.tubeRadiusAbsolute = 0.125
+PseudocolorAtts.tubeRadiusBBox = 0.005
+PseudocolorAtts.tubeRadiusVarEnabled = 0
+PseudocolorAtts.tubeRadiusVar = ""
+PseudocolorAtts.tubeRadiusVarRatio = 10
+PseudocolorAtts.tailStyle = PseudocolorAtts.NONE  # NONE, Spheres, Cones
+PseudocolorAtts.headStyle = PseudocolorAtts.NONE  # NONE, Spheres, Cones
+PseudocolorAtts.endPointRadiusSizeType = PseudocolorAtts.FractionOfBBox  # Absolute, FractionOfBBox
+PseudocolorAtts.endPointRadiusAbsolute = 0.125
+PseudocolorAtts.endPointRadiusBBox = 0.05
+PseudocolorAtts.endPointResolution = 10
+PseudocolorAtts.endPointRatio = 5
+PseudocolorAtts.endPointRadiusVarEnabled = 0
+PseudocolorAtts.endPointRadiusVar = ""
+PseudocolorAtts.endPointRadiusVarRatio = 10
+PseudocolorAtts.renderSurfaces = 1
+PseudocolorAtts.renderWireframe = 0
+PseudocolorAtts.renderPoints = 0
+PseudocolorAtts.smoothingLevel = 0
+PseudocolorAtts.legendFlag = 1
+PseudocolorAtts.lightingFlag = 1
+PseudocolorAtts.wireframeColor = (0, 0, 0, 0)
+PseudocolorAtts.pointColor = (0, 0, 0, 0)
+SetPlotOptions(PseudocolorAtts)
+# Begin spontaneous state
+View3DAtts = View3DAttributes()
+View3DAtts.viewNormal = (0.263884, -0.896668, -0.355459)
+View3DAtts.focus = (0, 0, 0)
+View3DAtts.viewUp = (0.947962, 0.173032, 0.267261)
+View3DAtts.viewAngle = 30
+View3DAtts.parallelScale = 190.526
+View3DAtts.nearPlane = -381.051
+View3DAtts.farPlane = 381.051
+View3DAtts.imagePan = (0, 0)
+View3DAtts.imageZoom = 1
+View3DAtts.perspective = 1
+View3DAtts.eyeAngle = 2
+View3DAtts.centerOfRotationSet = 0
+View3DAtts.centerOfRotation = (0, 0, 0)
+View3DAtts.axis3DScaleFlag = 0
+View3DAtts.axis3DScales = (1, 1, 1)
+View3DAtts.shear = (0, 0, 1)
+View3DAtts.windowValid = 1
+SetView3D(View3DAtts)
+# End spontaneous state
